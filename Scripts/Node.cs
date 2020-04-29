b745c5cfb12e8160bd7458f95b45c1542cba9b78
frozenparadox99 <frostbitecryengine300@gmail.com>
2020-04-29 19:52:41 +0530

    created Node class

---
+++ Scripts/Node.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum NodeType
+{
+    Open = 0,
+    Blocked =1
+}
+
+public class Node
+{
+    //Variable to see if the node is blocked or open for traversing or moving into
+    public NodeType nodeType = NodeType.Open;
+
+    //Variables to keep track of position in the grid
+    public int xIndex = -1;
+    public int yIndex = -1;
+
+    public Vector3 position;
+
+    //List to track neighbors
+    public List<Node> neighbors = new List<Node>();
+    public Node previous = null;
+
+    public Node(int xIndex,int yIndex,NodeType nodeType)
+    {
+        this.xIndex = xIndex;
+        this.yIndex = yIndex;
+        this.nodeType = nodeType;
+    }
+
+    public void Reset()
+    {
+        previous = null;
+    }
+}
