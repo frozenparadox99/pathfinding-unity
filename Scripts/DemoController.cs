42d90e6ed189be2447bdc4ee1b040ba141c33144
frozenparadox99 <frostbitecryengine300@gmail.com>
2020-04-30 10:52:30 +0530

    added controller to conform to MVC architecture

---
+++ Scripts/DemoController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoController : MonoBehaviour
+{
+    public MapData mapData;
+    public Graph graph;
+
+    private void Start()
+    {
+
+        if(mapData!=null && graph != null)
+        {
+            int[,] mapInstance = mapData.MakeMap();
+            graph.Init(mapInstance);
+
+            GraphView graphView = graph.gameObject.GetComponent<GraphView>();
+            if (graphView != null)
+            {
+                graphView.Init(graph);
+            }
+        }
+    }
+}
