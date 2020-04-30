5a9949632281915acbec0965aadb60b692bbc78d
frozenparadox99 <frostbitecryengine300@gmail.com>
2020-04-30 10:39:38 +0530

    added graphView

---
+++ Scripts/GraphView.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Graph))]
+public class GraphView : MonoBehaviour
+{
+    public GameObject nodeViewPrefab;
+
+    public Color baseColor = Color.white;
+    public Color wallColor = Color.black;
+
+    public void Init(Graph graph)
+    {
+        if(graph == null)
+        {
+            Debug.LogWarning("Graphview : No graph init");
+            return;
+        }
+
+        foreach(Node n in graph.nodes)
+        {
+            GameObject instance = Instantiate(nodeViewPrefab, Vector3.zero, Quaternion.identity);
+            NodeView nodeView = instance.GetComponent<NodeView>();
+
+            if (nodeView != null)
+            {
+                nodeView.Init(n);
+
+                if(n.nodeType == NodeType.Blocked)
+                {
+                    nodeView.ColorNode(wallColor);
+                }
+                else
+                {
+                    nodeView.ColorNode(baseColor);
+                }
+            }
+        }
+    }
+
+    
+}
